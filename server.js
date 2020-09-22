e5ddb286956979ad122a76e5122f0d66489a45a3
Muhammed Ekinci <siraceddinekinci@gmail.com>
2020-09-22 01:13:51 -0400

    fixed layout

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-var orm = require("./orm.js");
+
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 const consoleTable = require('console.table');
 
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "*Momaster1",
+  database: "pets_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
