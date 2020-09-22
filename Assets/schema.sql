a8d045c1327dd2897d079b2eb0c4b3036fb152d2
Muhammed Ekinci <siraceddinekinci@gmail.com>
2020-09-22 00:55:01 -0400

    fixed some database tables

--- Assets/schema.sql
+++ Assets/schema.sql
@@ -1,24 +1,33 @@
-CREATE SCHEMA `company_db` ;
+DROP DATABASE IF EXISTS employeeDb;
+CREATE database employeeDb;
 
-CREATE TABLE `company_db`.`employee` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(45) NULL,
-  `last_name` VARCHAR(45) NULL,
-  `role_id` INT NULL,
-  `manager_id` INT NULL,
-  PRIMARY KEY (`id`));
+USE employeeDb;
 
+CREATE TABLE departments (
+  id INT AUTO_INCREMENT NOT NULL,
+  name VARCHAR(30) NULL,
+  PRIMARY KEY (id)
+);
 
-CREATE TABLE `company_db`.`department` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`));
+CREATE TABLE role (
+  id INT AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30) NULL,
+  salary DECIMAL(10,2) NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES departments(id)
+);
 
-CREATE TABLE `company_db`.`role` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(45) NULL,
-  `salary` DECIMAL NULL,
-  `department_id` INT NULL,
-  PRIMARY KEY (`id`));
-
+CREATE TABLE employees (
+  id INT AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES departments(id),
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employees(id)
+);
 
