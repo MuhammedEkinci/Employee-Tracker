80a103a34176888be217317a88224e9a05294a0f
Muhammed Ekinci <siraceddinekinci@gmail.com>
2020-09-22 01:47:20 -0400

    added value to data tables

---
+++ Assets/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO departments (name) VALUES 
+("Engineering")
+
+
+INSERT INTO role (title, salary, department_id) VALUES 
+("Engineer", 100000, 1)
+
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id, department_id) VALUES
+("Muhammed", "Ekinci", 3, NULL, 1)
+
